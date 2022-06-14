a7dd172238d0cc5329a67d5572ff6ee16ac43126
Jason Roeckle <jroeckle@twitter.com>
2022-06-14 14:11:15 -0700

    * static icons

---
+++ src/config/StaticIconSettings.js
@@ -0,0 +1,32 @@
+const StaticIconSettings = {
+  Home: {
+    active: "IconHome.svg",
+    default: "IconHomeStroke.svg",
+  },
+  Explore: {
+    active: "IconHash.svg",
+    default: "IconHashStroke.svg",
+  },
+  Notifications: {
+    active: "IconNotifications.svg",
+    default: "IconNotificationsStroke.svg",
+  },
+  Messages: {
+    active: "IconMessages.svg",
+    default: "IconMessagesStroke.svg",
+  },
+  Bookmarks: {
+    active: "IconBookmark.svg",
+    default: "IconBookmarkStroke.svg",
+  },
+  Lists: {
+    active: "IconLists.svg",
+    default: "IconListsStroke.svg",
+  },
+  Profile: {
+    active: "IconPerson.svg",
+    default: "IconPersonStroke.svg",
+  },
+};
+
+export default StaticIconSettings;
--- src/config/index.js
+++ src/config/index.js
@@ -2,12 +2,14 @@ import LOTTIE_OPTIONS from "./LottieOptions";
 import PAGE_LOADER_OPTIONS from "./PageLoaderOptions";
 import NAV_CONFIG from "./NavConfig";
 import COMPONENT_ANIMATIONS from "./ComponentAnimations";
+import StaticIconSettings from "./StaticIconSettings";
 
 const CONFIG = {
   LOTTIE_OPTIONS,
   PAGE_LOADER_OPTIONS,
   NAV_CONFIG,
-  COMPONENT_ANIMATIONS
+  COMPONENT_ANIMATIONS,
+  StaticIconSettings
 };
 
 export default CONFIG;
