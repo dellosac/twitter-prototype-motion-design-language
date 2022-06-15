3bb44efeba19657ebcd077b30c022b950ce665ec
Jason Roeckle <jroeckle@twitter.com>
2022-06-15 13:27:59 -0700

    animated jitter fix for right bar

--- src/components/SidebarRight/SidebarRight.js
+++ src/components/SidebarRight/SidebarRight.js
@@ -4,109 +4,130 @@ import SearchIcon from "../../images/search-icon.png";
 import Transition from "../Transition";
 
 const SidebarRight = ({ loaderStyle, showLarryEntrance }) => {
-
   return (
-      <div className={styles.SidebarRight}>
-        <div className={styles.wrapper}>
-          <Transition 
-            isActive={showLarryEntrance}
-            animationConfiguration={loaderStyle}
-            entranceDelay={0 * loaderStyle.delayFactor}
-            component={"searchBar"}
-          >
-            <div className={styles.searchBar}>
-              <div className={styles.searchBarPill}>
-                <div className={styles.searchWrapper}>
-                  <img className={styles.searchIcon} src={SearchIcon} />
-                  <span className={`body ${styles.searchLabel}`}>Search Twitter</span>
-                </div>              
+    <div className={styles.SidebarRight}>
+      <div className={styles.wrapper}>
+        <Transition
+          isActive={showLarryEntrance}
+          animationConfiguration={loaderStyle}
+          entranceDelay={0 * loaderStyle.delayFactor}
+          component={"searchBar"}
+          entranceOnly={true}
+        >
+          <div className={styles.searchBar}>
+            <div className={styles.searchBarPill}>
+              <div className={styles.searchWrapper}>
+                <img className={styles.searchIcon} src={SearchIcon} />
+                <span className={`body ${styles.searchLabel}`}>
+                  Search Twitter
+                </span>
               </div>
             </div>
-          </Transition>
-          <Transition 
-            isActive={showLarryEntrance}
-            animationConfiguration={loaderStyle}
-            entranceDelay={1 * loaderStyle.delayFactor}
-            component={"whatsHappeningModule"}
-          >
-            <div className={styles.whatsHappeningModule}>
-              <Transition 
-                isActive={showLarryEntrance}
-                animationConfiguration={loaderStyle}
-                entranceDelay={1.5 * loaderStyle.delayFactor}
-                component={"whatsHappeningLabel"}
-              >
-                <h1 className={`headline1 ${styles.whatsHappeningLabel}`}>What's happening</h1>
-              </Transition>
-              <ul className={styles.whatsHappeningWrapper}>
-                {[...Array(5)].map((e, i) => {
-
-                  return (
-                    <Transition 
-                      key={`lil-${i}`}
-                      isActive={showLarryEntrance}
-                      animationConfiguration={loaderStyle}
-                      entranceDelay={(2 + i) * loaderStyle.delayFactor}
-                      hasSkeletalLoading={showLarryEntrance}
-                    >
-                      <li className={styles.whatsHappeningItem}>
-                        <StaticItem src={'./images/sidebar-right/whats-happening/item-' + (i + 1)} />
-                      </li>
-                    </Transition>
-                  );
-                })}
-              </ul>
-              <Transition 
-                isActive={showLarryEntrance}
-                animationConfiguration={loaderStyle}
-                entranceDelay={7 * loaderStyle.delayFactor}
-              >
-                <h4 className={`body ${styles.showMoreLabel}`}>Show more</h4> 
-              </Transition>
-            </div>
-          </Transition>
-          <Transition 
-            isActive={showLarryEntrance}
-            animationConfiguration={loaderStyle}
-            entranceDelay={8 * loaderStyle.delayFactor}
-            component={"whoToFollowModule"}
-          >
-            <div className={styles.whoToFollowModule}>
-              <Transition 
-                  isActive={showLarryEntrance}
+          </div>
+        </Transition>
+        <Transition
+          isActive={showLarryEntrance}
+          animationConfiguration={loaderStyle}
+          entranceDelay={1 * loaderStyle.delayFactor}
+          component={"whatsHappeningModule"}
+          entranceOnly={true}
+        >
+          <div className={styles.whatsHappeningModule}>
+            <Transition
+              isActive={showLarryEntrance}
+              animationConfiguration={loaderStyle}
+              entranceDelay={1.5 * loaderStyle.delayFactor}
+              component={"whatsHappeningLabel"}
+              entranceOnly={true}
+            >
+              <h1 className={`headline1 ${styles.whatsHappeningLabel}`}>
+                What's happening
+              </h1>
+            </Transition>
+            <ul className={styles.whatsHappeningWrapper}>
+              {[...Array(5)].map((e, i) => {
+                return (
+                  <Transition
+                    key={`lil-${i}`}
+                    isActive={showLarryEntrance}
                     animationConfiguration={loaderStyle}
-                    entranceDelay={8.5 * loaderStyle.delayFactor}
-                    component={"whoToFollowLabel"}
-              >
-                <h1 className="headline1">Who to follow</h1>
-              </Transition>
-              <ul className={styles.whoToFollowWrapper}>
-                  {[...Array(3)].map((e, i) => {
-                    return (
-                      <Transition 
-                        key={`lil-${i}`}
-                        isActive={showLarryEntrance}
-                        animationConfiguration={loaderStyle}
-                        entranceDelay={(9 + i) * loaderStyle.delayFactor}
-                      >
-                        <li>
-                          <StaticItem src={'./images/sidebar-right/who-to-follow/account-' + (i + 1)} />
-                        </li>
-                      </Transition>
-                    );
-                  })}
-                </ul>
-                <Transition 
-                  isActive={showLarryEntrance}
-                  animationConfiguration={loaderStyle}
-                  entranceDelay={10 * loaderStyle.delayFactor}
-                >
-                  <h4 className={`body ${styles.showMoreLabel}`}>Show more</h4> 
-                </Transition>
-            </div>
-          </Transition>
-        </div>
+                    entranceDelay={(2 + i) * loaderStyle.delayFactor}
+                    hasSkeletalLoading={showLarryEntrance}
+                    entranceOnly={true}
+                  >
+                    <li className={styles.whatsHappeningItem}>
+                      <StaticItem
+                        src={
+                          "./images/sidebar-right/whats-happening/item-" +
+                          (i + 1)
+                        }
+                      />
+                    </li>
+                  </Transition>
+                );
+              })}
+            </ul>
+            <Transition
+              isActive={showLarryEntrance}
+              animationConfiguration={loaderStyle}
+              entranceDelay={7 * loaderStyle.delayFactor}
+              entranceOnly={true}
+            >
+              <h4 className={`body ${styles.showMoreLabel}`}>Show more</h4>
+            </Transition>
+          </div>
+        </Transition>
+        <Transition
+          isActive={showLarryEntrance}
+          animationConfiguration={loaderStyle}
+          entranceDelay={8 * loaderStyle.delayFactor}
+          component={"whoToFollowModule"}
+          entranceOnly={true}
+        >
+          <div className={styles.whoToFollowModule}>
+            <Transition
+              isActive={showLarryEntrance}
+              animationConfiguration={loaderStyle}
+              entranceDelay={8.5 * loaderStyle.delayFactor}
+              component={"whoToFollowLabel"}
+              entranceOnly={true}
+            >
+              <h1 className="headline1">Who to follow</h1>
+            </Transition>
+            <ul className={styles.whoToFollowWrapper}>
+              {[...Array(3)].map((e, i) => {
+                return (
+                  <Transition
+                    key={`lil-${i}`}
+                    isActive={showLarryEntrance}
+                    animationConfiguration={loaderStyle}
+                    entranceDelay={(9 + i) * loaderStyle.delayFactor}
+                    entranceOnly={true}
+                  >
+                    <li>
+                      <StaticItem
+                        src={
+                          "./images/sidebar-right/who-to-follow/account-" +
+                          (i + 1)
+                        }
+                      />
+                    </li>
+                  </Transition>
+                );
+              })}
+            </ul>
+            <Transition
+              isActive={showLarryEntrance}
+              animationConfiguration={loaderStyle}
+              entranceDelay={10 * loaderStyle.delayFactor}
+              entranceOnly={true}
+            >
+              <h4 className={`body ${styles.showMoreLabel}`}>Show more</h4>
+            </Transition>
+          </div>
+        </Transition>
       </div>
+    </div>
   );
 };
 
